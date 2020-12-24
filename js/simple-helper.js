7858f93f49459c880830e3b0d21d6825d2ce2234
mejmon <72074632+mejmon@users.noreply.github.com>
2020-12-24 13:58:44 +0100

    förbättrat hemsidan

--- js/simple-helper.js
+++ js/simple-helper.js
@@ -1,7 +1,11 @@
-Helper.show = function (tag) {
+var Helper = {};
+
+Helper.show = function (tag, style) {
     document.getElementById(tag).style.display="block";
+    document.getElementById(style).style.color="black";
 }
 
-Helper.hide = function (tag) {
+Helper.hide = function (tag, style) {
     document.getElementById(tag).style.display="none";
+    document.getElementById(style).style.color = "white";
 }
