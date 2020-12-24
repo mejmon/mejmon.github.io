996a48223536be066b66b98f8a5b9cbbd463cda9
mejmon <72074632+mejmon@users.noreply.github.com>
2020-12-24 14:37:04 +0100

    fixat till menyn och lagt till kontakt

--- js/simple-helper.js
+++ js/simple-helper.js
@@ -3,9 +3,11 @@ var Helper = {};
 Helper.show = function (tag, style) {
     document.getElementById(tag).style.display="block";
     document.getElementById(style).style.color="black";
+    document.getElementById(style).style.background="#9698eb";
 }
 
 Helper.hide = function (tag, style) {
     document.getElementById(tag).style.display="none";
     document.getElementById(style).style.color = "white";
+    document.getElementById(style).style.background="#686af8";
 }
