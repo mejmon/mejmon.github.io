116cdf0f1be93e75250a21b6d4119d842fb05599
mejmon <72074632+mejmon@users.noreply.github.com>
2020-12-26 12:44:44 +0100

    upd vib

--- js/notification.js
+++ js/notification.js
@@ -5,7 +5,8 @@ async function showNotification() {
         const noti = new Notification('Hello!', {
             body: 'DOOOHHHHHHHH!',  //meddelande på push
             icon: 'image/simpsons.jfif', //bild som ska presenteras
-            vibrate: [100, 50, 100],   //tid och styrka på vibration i telefon (100ms, paus 50ms, sedan vibrera i 100ms.)
+            //vibrate: [100, 50, 100],   //tid och styrka på vibration i telefon (100ms, paus 50ms, sedan vibrera i 100ms.)
+            vibrate: [200, 100, 200, 100, 200, 100, 200],
             data: {  //data som vi från service worker kan jobba med
               dateOfArrival: Date.now(),  
               primaryKey: 1
