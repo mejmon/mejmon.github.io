1dac3158d755be4d05a0ef88bd09ad4f777e53c8
mejmon <72074632+mejmon@users.noreply.github.com>
2020-12-26 13:09:50 +0100

    upd noti

--- js/notification.js
+++ js/notification.js
@@ -1,9 +1,9 @@
-/*
+
 async function showNotification() {
     const result = await Notification.requestPermission();
     if (result === 'granted') {
-        const noti = new Notification('Hello!', {
-            body: 'DOOOHHHHHHHH!',  //meddelande på push
+        const noti = new Notification('DOOOOHHHHHH!', {
+            body: 'HELLO!',  //meddelande på push
             icon: 'image/simpsons.jfif', //bild som ska presenteras
             //vibrate: [100, 50, 100],   //tid och styrka på vibration i telefon (100ms, paus 50ms, sedan vibrera i 100ms.)
             vibrate: [200, 100, 200, 100, 200, 100, 200],
@@ -17,19 +17,5 @@ async function showNotification() {
 }
 showNotification();
 
-*/
 
-function showNotification() {
-  Notification.requestPermission(function(result) {
-    if (result === 'granted') {
-      navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          icon: 'image/simpsons.jfif',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
-      });
-    }
-  });
-}
+
